dd7fada83eaddfbb421454281bad273dd7cc2529
MominRhaman <mominrhaman007@gmail.com>
2024-08-08 15:07:53 +0600

    added jdon file

--- output/script.js
+++ output/script.js
@@ -1,3 +1,21 @@
 "use strict";
-const country = "I love Bangladesh";
-console.log(country);
+// function multiply(a : number, b: number) {
+//     return a * b;
+// }
+// console.log(multiply(10, 4));
+// array ==================================
+// let fruits = ["apple", "Lichu",  "banana"];
+// fruits.push("apple");
+// let mixed = ["apple", 5 , true , {}];
+// mixed.push({
+//     name: "Momin"
+// });
+// =====================================
+// Object +++
+let person = {
+    name: "Mashrafi",
+    age: 35,
+    isCaptain: true,
+};
+// person.name = "Momin"
+person.age = 35;
--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,32 @@
-const country = "I love Bangladesh";
+// function multiply(a : number, b: number) {
+//     return a * b;
+// }
 
-console.log(country);
+// console.log(multiply(10, 4));
 
+// array ==================================
+
+// let fruits = ["apple", "Lichu",  "banana"];
+
+
+// fruits.push("apple");
+
+// let mixed = ["apple", 5 , true , {}];
+
+// mixed.push({
+//     name: "Momin"
+// });
+
+// =====================================
+
+// Object +++
+
+let person = {
+    name: "Mashrafi",
+    age: 35,
+    isCaptain: true,
+};
+
+// person.name = "Momin"
+
+person.age = 35;
