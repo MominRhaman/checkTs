a6846da1d9bbe3a6f00db24f7a064fc1a7285231
MominRhaman <mominrhaman007@gmail.com>
2024-08-12 18:49:29 +0600

    added the class 7

--- output/script.js
+++ output/script.js
@@ -1,38 +1,12 @@
 "use strict";
-// function multiply(a : number, b: number) {
-//     return a * b;
-// }
-// console.log(multiply(10, 4));
-// array ==================================
-// let fruits = ["apple", "Lichu",  "banana"];
-// fruits.push("apple");
-// let mixed = ["apple", 5 , true , {}];
-// mixed.push({
-//     name: "Momin"
-// });
-// =====================================
-// Object +++
-// let person = {
-//     name: "Mashrafi",
-//     age: 35,
-//     isCaptain: true,
-// };
-// // person.name = "Momin"
-// person.age = 35;
-function add(n1, n2, showResult, phrase) {
-    // if (typeof n1 !== "number" || typeof n2 !== 'number') {
-    //     throw new Error('Incorrect input!');
-    // }
-    const result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-const number1 = 5; //5.0
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+// const person: {
+//     name: string;
+//     age: number;
+//     nickname: string;
+// } = {
+const person = {
+    name: 'Mr. Mominur Rhaman',
+    age: 27,
+    nickname: 'Momin'
+};
+console.log(person);
